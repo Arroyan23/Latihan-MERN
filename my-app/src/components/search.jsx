4268045065a2e387ce4bc2e44414c59caf9048d0
Arroyan23 <syawqiarroyan@gmail.com>
2024-10-14 13:46:08 +0700

    Menyelesaikan pekerjaan

--- my-app/src/components/search.jsx
+++ my-app/src/components/search.jsx
@@ -6,14 +6,27 @@ function Search(props) {
   const [search, setSearch] = useState("");
 
   const cetakInput = (event) => {
+    // yang fungsi ini untuk meng set value pada search sehingga ketauan
+    // apa yang dituliskan pada user pada tag html
     setSearch(event.target.value);
+  };
+
+  const cariReturnTombol = () => {
+    // karena yang ini bagian untuk mengembalikan ke nilai pada tag html tersebut
+    // maka yang tombol akan melakukan pengembalian pada props yang ada
+    // pada tag html tersebut
     props.onSetSearch(search);
   };
+  // coba menggunakan dia baru beraksi ketika tombolnya di klik
+  // lalu dia baru melakukan aksinya
   return (
     <>
-      <label htmlFor="">Cari Kata Kunci</label>
-      <input onChange={cetakInput}></input>
-      <p>Kata yang anda cari adalah : {search}</p>
+      Cari kata kunci : <input onChange={cetakInput}></input>
+      <button onClick={cariReturnTombol}>Cari Kata Kunci</button>
+      <p>
+        Data yang Ditemukan : {props.countFindedData}, Kata yang anda cari
+        adalah : {search}
+      </p>
     </>
   );
 }
--- my-app/src/pages/hompage.jsx
+++ my-app/src/pages/hompage.jsx
@@ -1,21 +1,35 @@
 //  halaman untuk homepage
 import { useState } from "react";
-import Article from "../components/kebenaran";
+import Kebenaran from "../components/kebenaran";
 import Search from "../components/search";
 import postData from "../favian.json";
 function Homepage() {
-  const [filtered, setFilter] = useState("");
+  const [filtered, setFilter] = useState(postData);
+  // menghitung berapa banyak data yang ditemukan
+  const [countData, setCountData] = useState(0);
   const takeSearchValue = (value) => {
     // untuk filter di bagian value dari searchnya itu sendiri
-    const filteredJSON = postData.filter((elements) =>
-      elements.tags.includes(value)
-    );
+    const filteredJSON = postData.filter((e) => e.tags.includes(value));
     setFilter(filteredJSON);
+    setCountData(filteredJSON.length);
   };
   return (
     <>
-      <Search onSetSearch={takeSearchValue} />
-      
+      <Search onSetSearch={takeSearchValue} countFindedData={countData} />
+      {filtered.map((elements) => {
+        // jangan lupa untuk me return valuenya agar terlihat ke dalam monitoring
+        // layarnya itu sendiri
+        return (
+          <>
+            <Kebenaran
+              nama={elements.nama}
+              hobby={elements.hobby}
+              kasus={elements.kasus}
+              tags={elements.tags}
+            />
+          </>
+        );
+      })}
     </>
   );
 }
